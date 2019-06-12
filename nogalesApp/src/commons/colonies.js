8f351f62c242e601f96242370a0165040f48de83
aldairmh98 <39748772+aldairmh98@users.noreply.github.com>
2019-06-12 16:54:52 -0500

    nona
    onso

--- nogalesApp/src/commons/colonies.js
+++ nogalesApp/src/commons/colonies.js
@@ -1,4 +1,5 @@
 export const colonies = [
     {topic: 'NOGALES', name: 'Nogales'},
-    { topic: 'GIO', name:'La colonia del Gio'}
+    { topic: 'GIO', name:'La colonia del Gio'},
+    { topic: 'GIO2', name:'La colonia del Gio2'}
 ]
