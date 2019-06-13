a20a38c274fc55c5f16e7eb36d675e37a2d5dc04
aldairmh98 <aldairmh98@outlook.es>
2019-06-13 14:54:31 -0500

    Working the setup, creo jaja

--- nogalesApp/src/views/home.js
+++ nogalesApp/src/views/home.js
@@ -21,113 +21,110 @@ class Home extends Component {
 
     onDocUpdate = (querySnapshot) => {
         const { latitud, longitud } = querySnapshot.data();
-        this.setState({ busLocation: { latitude: latitud, longitude:longitud } });
+        this.setState({ busLocation: { latitude: latitud, longitude: longitud } });
     }
 
     componentDidMount() {
         //this.firebaseRef = firebase.firestore().collection('zones').doc('sMhGAj5dyvZ7pFRORl4t');
         //this.unsubscribe = this.firebaseRef.onSnapshot(this.onDocUpdate);
+        this.startTracking();
+    }
+
+    startTracking() {
         getColonie().then(
             value => {
                 if (value === null) {
                     this.setState({ showInitialConfiguration: true });
                 } else {
-                    this.setState({ colonie: value });
-                    this.startTracking();
-                }
+                    this.setState({ fetching: true });
+                    this.collectionRef = firebase.firestore().collection('zones').where('colonies', 'array-contains', value).get().then(
+                        collection => {
+                            collection.forEach(
+                                document => {
+                                    this.unsubscribe = document.ref.onSnapshot(this.onDocUpdate);
+                                    this.setState({ fetching: false })
+                                }
+                            )
+                        }
+                    ).catch(
+                        err => {
+                            this.setState({ fetching: false })
+                        }
+                    )
+            }
             }
         ).finally(
-            onfinally => {
-                this.setState({ starting: false })
+                onfinally => {
+                    this.setState({ starting: false })
             }
         );
     }
 
-    startTracking() {
-        if (!this.state.showInitialConfiguration && this.state.colonie) {
-            this.setState({fetching: true});
-            this.collectionRef = firebase.firestore().collection('zones').where('colonies', 'array-contains', this.state.colonie).get().then(
-                collection => {
-                    collection.forEach(
-                        document => {
-                            this.unsubscribe = document.ref.onSnapshot(this.onDocUpdate);
-                            this.setState({fetching: false})
-                        }
-                    )
-                }
-            ).catch(
-                err => {
-                    this.setState({fetching: false})
-                }
-            )
+componentWillUnmount() {
+    this.setState({ showInitialConfiguration: false })
+    this.unsubscribe();
+}
+
+render() {
+    if (this.state.starting) {
+        return (
+            <View style={styles.container}>
+                <Spinner></Spinner>
+                <Text>{this.state.fetching ? 'Buscando si ha salido autobús' : 'Comprobando configuración'} </Text>
+            </View>
+        )
+    } else {
+        if (this.state.showInitialConfiguration) {
+            return <Modal visible={this.state.showInitialConfiguration}>
+                <View style={{ flex: 1 }}>
+                    <Colonies onSetted={() => {
+                        this.setState({ showInitialConfiguration: false });
+                        this.startTracking();
+                        alert('Ahora recibirás notificaciones.')
+                    }}></Colonies>
+                </View>
+            </Modal>
+
         }
-    }
-
-    componentWillUnmount() {
-        this.setState({showInitialConfiguration: false})
-        this.unsubscribe();
-    }
-
-    render() {
-        if (this.state.starting) {
+        if (this.state.fetching) {
             return (
                 <View style={styles.container}>
                     <Spinner></Spinner>
-                    <Text>{this.state.fetching ?'Buscando si ha salido autobús':'Comprobando configuración' } </Text>
-                </View>
-            )
-        } else {
-            if (this.state.showInitialConfiguration) {
-                return <Modal visible={this.state.showInitialConfiguration}>
-                    <View style={{ flex: 1 }}>
-                        <Colonies onSetted={() => {
-                            this.setState({ showInitialConfiguration: false });
-                            this.startTracking();
-                            alert('Ahora recibirás notificaciones.')
-                        }}></Colonies>
-                    </View>
-                </Modal>
-
-            }
-            if(this.state.fetching){
-                return(
-                    <View style={styles.container}>
-                    <Spinner></Spinner>
                     <Text>Buscando si ha salido autobús</Text>
                 </View>
-                )
-            }
-            if (!this.state.busLocation){
-                return <View>
-                    <Text>
-                        Autobus no pasa por su colonia, sorry
-                    </Text>
-                </View>
-            }
+            )
         }
-        return (
-            <View style={styles.container}>
-                <MapView
-                    showsUserLocation={true}
-                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                    style={styles.map}
-                    ref = {ref => this.map = ref}
-                    initialRegion={{
-                        latitude: 18.8157483,
-                        longitude: -97.1640488,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
-                    }}
-
-                >
-                    {this.state.busLocation ? <Marker image={busIcon} coordinate={this.state.busLocation}></Marker> : null}
-                </MapView>
-                <Button onPress={() => {
-                    this.map.fitToCoordinates([this.state.busLocation])
-                }} title='Ubícame'></Button>
+        if (!this.state.busLocation) {
+            return <View>
+                <Text>
+                    Autobus no pasa por su colonia, sorry
+                    </Text>
             </View>
-        );
+        }
     }
+    return (
+        <View style={styles.container}>
+            <MapView
+                showsUserLocation={true}
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                ref={ref => this.map = ref}
+                initialRegion={{
+                    latitude: 18.8157483,
+                    longitude: -97.1640488,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+
+            >
+                {this.state.busLocation ? <Marker image={busIcon} coordinate={this.state.busLocation}></Marker> : null}
+            </MapView>
+            <Button onPress={() => {
+                this.map.fitToCoordinates([this.state.busLocation])
+            }} title='Ubícame'></Button>
+        </View>
+    );
+}
 }
 
 const styles = StyleSheet.create({
